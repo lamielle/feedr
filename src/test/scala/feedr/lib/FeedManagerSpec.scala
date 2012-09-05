2bb4659769db1032113e7ab0df3d03afe4634ec3
Alan LaMielle <alamielle@vmware.com>
2012-09-04 22:09:55 -0600

    Adds a few tests for FeedManager
    Adds tests for getting the ID and feed that a FeedManager manages.

--- src/test/scala/feedr/lib/FeedManagerSpec.scala
+++ src/test/scala/feedr/lib/FeedManagerSpec.scala
@@ -2,32 +2,36 @@ package feedr.lib
 
 import org.scalatest.FlatSpec
 import org.scalatest.matchers.ShouldMatchers
+import feedr.lib.FeedsManager.{RequestFeedManager, NewFeed}
+import feedr.lib.FeedManager.{RequestFeed, RequestFeedId}
+import net.liftweb.common.Full
+import feedr.model.Feed
 
-class FeedManagerSpec extends FlatSpec with ShouldMatchers {
-   /*
-   "shortestUniqueString" should "handle both empty inputs" in {
-      FeedsManager.shortestUniqueString("", Set()) should equal(None)
+class FeedManagerSpec extends FlatSpec with FeedrDBSetup with ShouldMatchers {
+   // Create a new feed and return its ID and manager
+   def newFeedAndManager = {
+      FeedsManager !! NewFeed() match {
+         case Full(feedId: Long) =>
+            FeedsManager !! RequestFeedManager(feedId) match {
+               case Full(Full(feedManager: FeedManager)) =>
+                  (feedId, feedManager)
+               case other => fail("Unexpected response from feed manager request: %s".format(other))
+            }
+         case other => fail("Unexpected response from new feed request: %s".format(other))
+      }
    }
 
-   "shortestUniqueString" should "handle an empty input string" in {
-      FeedsManager.shortestUniqueString("", Set("a")) should equal(None)
-      FeedsManager.shortestUniqueString("", Set("a", "b")) should equal(None)
+   "FeedManager" should "provide the ID of the feed it manages" in {
+      // TODO: look up syntax for tuple unpacking
+      val feedIdAndManager = newFeedAndManager
+      feedIdAndManager._2 !! RequestFeedId() should equal (feedIdAndManager._1)
    }
 
-   "shortestUniqueString" should "handle an empty input set" in {
-      FeedsManager.shortestUniqueString("a", Set()) should equal(Some("a"))
-      FeedsManager.shortestUniqueString("abcd", Set()) should equal(Some("a"))
+   it should "provide the feed that it manages" in {
+      val feedIdAndManager = newFeedAndManager
+      feedIdAndManager._2 !! RequestFeed() match {
+         case Full(Full(feed: Feed)) => feed.id should equal (feedIdAndManager._1)
+         case other => fail("No feed provided: %s".format(other))
+      }
    }
-
-   "shortestUniqueString" should "work properly" in {
-      FeedsManager.shortestUniqueString("a", Set("a")) should equal(None)
-      FeedsManager.shortestUniqueString("ab", Set("a", "b", "c")) should equal(Some("ab"))
-      FeedsManager.shortestUniqueString("a", Set("c", "b", "c")) should equal(Some("a"))
-      FeedsManager.shortestUniqueString("abcdef", Set("d", "b", "c")) should equal(Some("a"))
-      FeedsManager.shortestUniqueString("abcdef", Set("abcd", "basdf", "csdf")) should equal(Some("abcde"))
-      FeedsManager.shortestUniqueString("abc", Set("abc", "bd", "c")) should equal(None)
-      //FeedsManager$.shortestUniqueString("7f2a4745-5d02-4ec3-98cc-c8b964c97c3f", Set("e", "34", "4", "9", "4f", "d6", "2", "7")) should equal (Some("7f"))
-      //FeedsManager$.shortestUniqueString("7f2a4745-5d02-4ec3-98cc-c8b964c97c3f", Set("e", "34", "4", "9", "4f", "d6", "c1", "6", "1", "39", "b", "0", "2", "0a", "c", "30", "7", "3", "d")) should equal (Some("7f"))
-   }
-   */
 }
